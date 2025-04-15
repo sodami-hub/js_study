1ccd63fcf592b41e9eac28370918de5c3fe52b43
good4ram@naver.com <good4ram@naver.com>
2025-04-15 11:19:28 +0900

    250415 study_js +

--- study_note/example.js
+++ study_note/example.js
@@ -1,16 +1,4 @@
-const arrayLike = {
-    0:1,
-    1:2,
-    2:3,
-    length:3
-};
+const user = {name:'lee', age:30};
+const {name:a,age:b} = user;
 
-// 유사 배열 객체는 length 프로퍼티를 갖기 때문에 for 문으로 순회 가능
-for (let i = 0; i< arrayLike.length; i++) {
-    console.log(arrayLike[i]);
-}
-
-// 이터러블이 아니기 때문에 for...of 문 사용 불가
-for (const item of arrayLike) {
-    console.log(item)
-}
+console.log(a,b); // lee 30
