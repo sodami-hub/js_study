068a311788d8be7265b918ddc2d2980d0dae6c8c
good4ram@naver.com <good4ram@naver.com>
2025-05-31 17:38:19 +0900

    250531 study_ts +

--- 타입스크립트_교과서/exam.ts
+++ 타입스크립트_교과서/exam.ts
@@ -20,3 +20,84 @@ const func2 = ():void => {
 const func3:()=>void = ()=>{
   const p = 3;
 }
+
+interface Person {
+  name: string;
+  age: number;
+  married: boolean;
+}
+
+const man:Person = {
+  name:'lee',
+  age:12,
+  married:true,
+}
+
+interface Func {
+  (x:number, y:number):number;
+}
+const add:Func=(x,y)=>x+y;
+
+interface Arr {
+  length:number;
+  [key:number]:number;
+}
+const arr:Arr =[1,2,3,4,5];
+console.log(arr.length);
+
+const arr2:Array<number> = [1,2,3,4,5];
+
+interface Merge {
+  one: string;
+}
+interface Merge {
+  two: number
+}
+
+const exam:Merge = {
+  one:'1',
+  two:2,
+}
+
+console.log(exam);
+
+namespace Example {
+  export interface Inner {
+    test:string;
+  }
+  export type test2=number;
+}
+
+const ex01:Example.Inner= {
+  test:'str'
+}
+const ex02:Example.test2 = 123;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
