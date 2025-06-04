19ff726d3bd61da080239cfff1e8f9a5fe2a10cb
good4ram@naver.com <good4ram@naver.com>
2025-06-04 12:14:08 +0900

    250604 study_ts +

--- 타입스크립트_교과서/exam.ts
+++
@@ -1,8 +0,0 @@
-interface Original {
-  name: string;
-  age: number;
-  married: boolean;
-}
-type Copy = {
-  readonly [key in keyof Original]? : Original[key]; // Initial type : {name: string, age: number, married: boolean}
-}
