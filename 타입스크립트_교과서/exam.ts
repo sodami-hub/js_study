caee83e5295199bf7f60b81c0be4ee904fd4d32f
good4ram@naver.com <good4ram@naver.com>
2025-06-09 11:57:36 +0900

    250609 study_ts +

--- 타입스크립트_교과서/exam.ts
+++ 타입스크립트_교과서/exam.ts
@@ -1,4 +1,25 @@
-type Add01 = (x:number, y:number) => number;
-type Add02 = (x:string, y:string) => string;
-type Add = Add01 & Add02;
-const add:Add = (x:any,y:any)=>x+y;
+enum Money {
+  WON,
+}
+enum Liter {
+  LITER,
+}
+
+interface M {
+  type: Money.WON,
+}
+interface L {
+  type: Liter.LITER,
+}
+
+function moneyOrLiter(param:M|L) {
+  if (param.type === Money.WON) {
+    console.log('Money')
+    return param;
+  } else {
+    console.log('Liter')
+    return param;
+  }
+}
+moneyOrLiter({type: Money.WON})  // Money
+moneyOrLiter({type: Liter.LITER})  // Money
